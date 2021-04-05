57e66a7c6463dab031a9cc0d83782e6b7e664aee
Cristian Penteado <boss@cristianpenteado.com.br>
2021-04-05 12:59:50 -0300

    split css in components, #4

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@ const app = new Vue({
     el: '#app',
     data: {
         lightTheme: 'colors-light',
-        rotateIcon: 'rotate'
+        rotateIcon: 'rotate',
+        imageDark: 'character-image-dark'
     },
     methods: {
         setTheme(){
             this.$refs.app.classList.toggle(this.lightTheme)
             this.$refs.icon.classList.toggle(this.rotateIcon)
-            this.$refs.image.classList.toggle('character-image-dark')
+            this.$refs.image.classList.toggle(this.imageDark)
         }
     },
 })
